fb0cf2569f40c1ac0154c11c25887da75b64ad42
Shahzad Ahamad <shahzadahamad285@gmail.com>
2026-05-04 23:06:51 +0530

    Added migration rn scripts

--- StockTrading.Apis/Program.cs
+++ StockTrading.Apis/Program.cs
@@ -1,6 +1,7 @@
 using StockTrading.Services;
 using StockTrading.IServices;
 using StockTrading.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +31,12 @@ builder.Services.AddTransient<IBrokerService>(serviceProvider =>
 builder.Services.AddHostedService<ScalpingService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<StockTradingDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
